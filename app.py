b2da395781515ece7c925063d47f934b7169866d
hwu8989 <hwu8989@gmail.com>
2016-10-06 17:23:41 -0700

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def contacts():
     try:
         my_list = []
         if cur != None:
-            cur.execute("""SELECT name from salesforce.contact""")
+            cur.execute("""SELECT name from salesforce.account""")
             rows = cur.fetchall()
             response = ''
             
